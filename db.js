7617279bacede2db6066585152081b0b8cd1007a
victor <victorcustodio845@gmail.com>
2023-07-24 03:40:28 -0300

    terminando a emplementaco com o db

--- db.js
+++ db.js
@@ -1,29 +1,57 @@
 // db da aplicacao
 
-
-
-async function connection(){
+async function connect(){
 
     if(global.connection && global.connection.state !== 'disconnected')
         return global.connection;
 
-
     const mysql = require("mysql2/promise");
     // local de connection
-    const connection = await mysql.createConnection("mysql://root:root@localhost:3306/db_clicksolid");
+    //const connection = await mysql.createConnection("mysql://root:root@localhost:3306/db_clicksolid");
+    const connection =  mysql.createConnection({
+        host: 'localhost', 
+        port: 3306,
+        user: 'root',
+        database: 'db_clicksolid',
+        password: ''
+    });
     console.log("conectado ao mysql");
     // variavel global que vai guardar as connections
-    global.connection;
+    global.connection = connection;
 
     return connection;
 }
 // mantem connection com o db
-connection();
+//connection();
 
 async function selectAluno(){
-    const conn = await connection();
-    return await conn.query('SELECT * FROM cl');
+    const conn = await connect();
+    const [rows] = await conn.query('SELECT * FROM alunos');
+    return rows
 }
 
-module.exports = {selectAluno}
+// funcao para acrecentar dados ao banco de dados
+async function insertAluno(alunos) {
+    const conn = await connect();
+    const sql = 'INSERT INTO alunos(id_alunos, nome_do_aluno, idade_do_aluno) VALUES(?, ?, ?);';
+    const values = [alunos.id_alunos, alunos.nome_do_aluno, alunos.idade_do_aluno];
+    return await conn.query(sql, values);
+}
+//funcao para alterar o aluno do db
+
+async function updateAlunos(id_alunos, alunos) {
+    const conn = await connect();
+    const sql =  'UPDATE alunos SET nome_do_aluno=?, idade_do_aluno=? WHERE id_aluno=?';
+    const values = [alunos.id_alunos, alunos.nome_do_aluno, alunos.idade_do_aluno, id_alunos];
+    return await conn.query(sql, values)
+}
+
+// funcao feita para deletar os alunos solicitado
+async function deleteAluno(id_aluno) {
+    const conn = await connect();
+    const sql = 'DELETE FROM alunos WHERE id_aluno=?;';
+    return await conn.query(sql, [id_aluno]);
+}
+
+module.exports = {selectAluno, insertAluno, updateAlunos, deleteAluno}
 
--- index.js
+++ index.js
@@ -2,38 +2,75 @@
 (async () => {
     const db = require("./db");
     console.log("conectou");
+    // nao coloquei select * from para nao atrapalar os DBA da empresa, isso pode dar ruim
+    // em um banco de dados muito grande
     console.log("SELECT * FROM alunos");
     const alunos = await db.selectAluno();
     console.log(alunos);
+
+    const insert = await db.insertAluno();
+    console.log(insert);
+
+    const delet = await db.deleteAluno();
+    console.log(delet);
+
+    const up = await db.updateAlunos();
+    console.log(up);
+
 })();
-
-
 
 
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 //porta de entrada, caminho e app
 const port = 3000;
-const path = ('path');
+var path = require('path');
+const { connect } = require("http2");
+const { connected } = require("process");
 const app = express();
+
+var login = "admin";
+var password = "12345678";
 
 // comando feito para acessar o assencion. com a senha de acesso
 app.use(session({secret:'admin'}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 //comandos de renderizacao
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '__views'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res)=> {
-    req.render('index');
+app.post('/post-test',(res, req) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+app.post('/',(req, res)=>{
+    console.log(req.body.login);
+    res.render('index')
+});
+app.get('/',(req, res)=> {
+    if(req.session.login){
+        res.render('logado');
+    }else {
+        req.render('index');
+    }
+    
 });
 app.post('/', (res, req)=>{
+    if(req.body.password == password && req.body.login == login) {
+        // logado com sucesso
+        req.session.login = login;
+        res.redirect('/');
+    }
     res.render('index')
 })
 
-app.listen(port, ()=> {
-    consolle.log('servidor na nuvem');
+
+app.listen(port,()=> {
+    console.log('servidor na nuvem');
 });
